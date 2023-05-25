2faf3a8939929eb97656db33b0654058556efa2a
essam <essam@34ml.com>
2023-05-25 15:41:47 +0300

    Add translation file

---
+++ resources/lang/en/en.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'en_title' => 'Title [en]',
+    'ar_title' => 'Title [ar]',
+    'en_description' => 'Description [en]',
+    'ar_description' => 'Description [ar]',
+    'en_keywords' => 'Keywords [en]',
+    'ar_keywords' => 'Keywords [ar]',
+    'follow' => 'Follow',
+    'image' => 'Image',
+];
